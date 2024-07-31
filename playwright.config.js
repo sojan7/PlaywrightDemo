a321ec09ffd115a0af5016a2cff465155bc23bc2
Sojan Somarajan <sojan_somarajan@epam.com>
2024-07-31 18:58:25 +0530

    File CleanUp

--- playwright.config.js
+++ playwright.config.js
@@ -38,14 +38,14 @@ module.exports = defineConfig({
       use: { ...devices["Desktop Chrome"] },
     },
 
-    /*{
+    {
       name: "firefox",
       use: { ...devices["Desktop Firefox"] },
     },
 
-    /* {
-      name: 'webkit',
-      use: { ...devices['Desktop Safari'] },
+    {
+      name: "webkit",
+      use: { ...devices["Desktop Safari"] },
     },
 
     /* Test against mobile viewports. */
